93ea6973747dc0fa9a2a74ec17bd2b588ba5be81
Jonathan Nilsfors <jonathan.nilsfors@gmail.com>
2016-12-04 19:35:57 -0500

    Endpoint for creating user. Hash password before create.

--- models/users.js
+++ models/users.js
@@ -1,10 +1,20 @@
 "use strict";
 
+var bcrypt = require('bcrypt-nodejs')
+
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
-    username: DataTypes.STRING
-  },
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
   });
+
+  User.beforeCreate(function(user, options) {
+    console.log('In the before create')
+    var hash = bcrypt.hashSync(user.password, null, null)
+        console.log('THIS IS THE HASH', hash)
+        user.password = hash;
+        console.log('USER', user)
+})
 
   return User;
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/create', function(req, res, next){
+    if(!req.body.email){
+        res.status(400).send('Need to provide an email.')
+    }
+    if(!req.body.password){
+        res.status(400).send('Need to privide a password.')
+    }
+    models.User.create({email: req.body.email}).success(function(user, created){
+        console.log('req.body.email', req.body.email)
+        console.log('CREATED', created)
+        console.log(user)
+        res.send('Created user')
+    })
 });
 
 module.exports = router;
