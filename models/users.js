237b266a47bf3a0b313cc3d7c452ecae7fa4c625
Jonathan Nilsfors <jonathan.nilsfors@gmail.com>
2016-12-04 21:57:17 -0500

    Use password hash for hashing password.

--- models/users.js
+++ models/users.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var bcrypt = require('bcrypt-nodejs')
+var passwordHash = require('password-hash')
 
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
@@ -11,12 +11,20 @@ module.exports = function(sequelize, DataTypes) {
     password: {
         type: DataTypes.STRING,
     }
+  },
+  {
+      instanceMethods: {
+        validPassword: function(password){
+            return passwordHash.verify(password, this.password)
+        }
+      }
   });
 
-  User.beforeCreate(function(user, options) {
-    var hash = bcrypt.hashSync(user.password, null, null)
+    User.beforeCreate(function(user, options) {
+        console.log(user.password)
+        var hash = passwordHash.generate(user.password)
         user.password = hash;
-})
+    })
 
   return User;
 };
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
+var jwt = require('jsonwebtoken')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -22,9 +23,28 @@ router.post('/create', function(req, res, next){
     if(req.body.password != req.body.confirm_password){
         return res.status(400).send('Passwords do not match!')
     }
-    models.User.create({email: req.body.email}).then(function(user, created){
-        return res.status(201).send('Created user')
+    models.User.create({email: req.body.email, password: req.body.password}).then(function(user, created){
+        res.status(201).send('Created user')
     })
 });
+
+router.post('/login', function(req, res, next){
+    console.log('Login')
+    if(!req.body.email){
+        return res.status(400).send('Need to provide an email')
+    }
+    if(!req.body.password){
+        return res.status(400).send('Need to provide a password')
+    }
+
+    models.User.find({where: {email: req.body.email}}).then(function(user){
+        if(!user.validPassword(req.body.password)){
+            return res.status(401).send('Invalid password.')
+        }
+        var token = jwt.sign({id: user.id}, 'secretkey', {expiresIn: "1d"})
+
+        return res.status(200).send({token: token})
+    })
+})
 
 module.exports = router;
